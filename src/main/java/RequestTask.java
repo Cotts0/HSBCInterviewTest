dce453a6cf07f9bf790cd5c7facaebddb09588b3
Cotts <cotts@qq.com>
2024-05-18 12:49:14 +0800

    get thread result

--- src/main/java/RequestTask.java
+++ src/main/java/RequestTask.java
@@ -1,32 +1,32 @@
-import java.util.Objects;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 
-public class RequestTask extends Thread {
+public class RequestTask  implements Callable<String> {
     private String url;
     private CountDownLatch latch;
-
-    @Override
-    public void run() {
-        try {
-            // 请求接口
-            String result = HSBCHttpUtils.doGet(url);
-            if (Objects.nonNull(result)) {
-                System.out.println("display the request like this --->");
-                System.out.println(result);
-            }
-        } catch (Exception e) {
-            System.out.println("Catch business exception," + e.getMessage());
-        } finally {
-            latch.countDown();
-        }
-    }
-
 
     public void setUrl(String url) {
         this.url = url;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
     public void setLatch(CountDownLatch latch) {
         this.latch = latch;
+    }
+
+    @Override
+    public String call() throws Exception {
+        try {
+            // 请求接口
+            return HSBCHttpUtils.doGet(url);
+        } catch (Exception e) {
+            System.out.println("Catch business exception," + e.getMessage());
+        } finally {
+            latch.countDown();
+        }
+        return null;
     }
 }
